f90c1d47bfd9a2c90c5b44697204516ba3ee98a7
Jason Stankevich <jason.stankevich@gmail.com>
2020-10-13 14:58:13 -0400

    vercel deployment changes

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -2,13 +2,13 @@ import config from '../config';
 
 const TokenService = {
   saveAuthToken(token) {
-    window.localStorage.setItem(config.TOKEN_KEY, token);
+    window.localStorage.setItem(config.API_KEY, token);
   },
   getAuthToken() {
-    return window.localStorage.getItem(config.TOKEN_KEY);
+    return window.localStorage.getItem(config.API_KEY);
   },
   clearAuthToken() {
-    window.localStorage.removeItem(config.TOKEN_KEY);
+    window.localStorage.removeItem(config.API_KEY);
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken();
